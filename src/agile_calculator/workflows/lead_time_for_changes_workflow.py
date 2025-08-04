a89b336191f124dbeac07efef8817f1cace9e835
KentFujii <kent.where.the.light.is@gmail.com>
2025-08-04 13:31:19 +0900

    LeadTimeForChangesWorkflowの初期化メソッドにsince_daysとusersの引数を追加し、extractメソッドでこれらの引数を使用するように変更しました。また、テストケースを更新しました。

--- src/agile_calculator/workflows/lead_time_for_changes_workflow.py
+++ src/agile_calculator/workflows/lead_time_for_changes_workflow.py
@@ -6,13 +6,15 @@ from ..transformers.lead_time_for_changes_transformer import (
 
 
 class LeadTimeForChangesWorkflow:
-    def __init__(self, github_token, repo_name):
+    def __init__(self, github_token, repo_name, since_days=None, users=None):
         self.extractor = GitHubExtractor(github_token)
         self.repo_name = repo_name
+        self.since_days = since_days
+        self.users = users
 
     def run(self):
         # データ抽出
-        pull_requests = list(self.extractor.extract(self.repo_name))
+        pull_requests = list(self.extractor.extract(self.repo_name, users=self.users, since_days=self.since_days))
         # 変換
         records = [LeadTimeForChangesTransformer(pr).transform() for pr in pull_requests if pr.merged_at]
         # ロード
