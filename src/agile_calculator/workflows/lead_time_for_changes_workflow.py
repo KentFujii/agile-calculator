53f88c09e1a0b9487927bb66ffde887a51218ddb
KentFujii <kent.where.the.light.is@gmail.com>
2025-08-08 00:30:32 +0900

    Refactor code structure for improved readability and maintainability

--- src/agile_calculator/workflows/lead_time_for_changes_workflow.py
+++ src/agile_calculator/workflows/lead_time_for_changes_workflow.py
@@ -1,4 +1,4 @@
-from ..extractors.github_extractor import GitHubExtractor
+from ..extractors.github.pull_request_extractor import PullRequestExtractor
 from ..loaders.matplotlib_loader import MatplotlibLoader
 from ..transformers.lead_time_for_changes_transformer import (
     LeadTimeForChangesTransformer,
@@ -7,17 +7,29 @@ from ..transformers.lead_time_for_changes_transformer import (
 
 class LeadTimeForChangesWorkflow:
     def __init__(self, github_token, repo_name, since_days=None, users=None):
-        self.extractor = GitHubExtractor(github_token)
+        self.github_token = github_token
         self.repo_name = repo_name
-        self.since_days = since_days
-        self.users = users
+        self.since_days = since_days if since_days is not None else 14
+        self.users = users if users is not None else []
 
     def run(self):
-        # データ抽出
-        pull_requests = list(self.extractor.extract(self.repo_name, users=self.users, since_days=self.since_days))
-        # 変換
-        records = [LeadTimeForChangesTransformer(pr).transform() for pr in pull_requests if pr.merged_at]
-        # ロード
-        loader = MatplotlibLoader(records)
-        loader.load()
-        return records
+        self._load(
+            self._transform(
+                self._extract()
+            )
+        )
+
+    def _extract(self):
+        extracted_records = PullRequestExtractor(self.github_token).extract(
+            self.repo_name, since_days=self.since_days, users=self.users
+        )
+        return extracted_records
+
+    def _transform(self, extracted_records):
+        transformed_records = [
+            LeadTimeForChangesTransformer(pr).transform() for pr in extracted_records if pr.merged_at
+        ]
+        return transformed_records
+
+    def _load(self, transformed_records):
+        MatplotlibLoader(transformed_records).load()
