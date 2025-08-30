b22ec5703663166b0783d44982075c6e0d560cec
KentFujii <kent.where.the.light.is@gmail.com>
2025-08-30 14:41:51 +0900

    fix: クラス名のスペルミスを修正

--- agile_calculator/tasks/transformers/passthrough_transformer.py
+++ agile_calculator/tasks/transformers/passthrough_transformer.py
@@ -1,6 +1,6 @@
 from agile_calculator.records.extracted.pull_request_record import PullRequestRecord
 
 
-class PassThroughTransformer:
+class PassthroughTransformer:
     def __init__(self, records: PullRequestRecord):
         self.records = records
--- tests/tasks/transformers/test_passthrough_transformer.py
+++ tests/tasks/transformers/test_passthrough_transformer.py
@@ -1,12 +1,12 @@
 from unittest.mock import MagicMock
-from agile_calculator.tasks.transformers.passthrough_transformer import PassThroughTransformer
+from agile_calculator.tasks.transformers.passthrough_transformer import PassthroughTransformer
 from agile_calculator.records.extracted.pull_request_record import PullRequestRecord
 
-class TestPassThroughTransformer:
+class TestPassthroughTransformer:
     def test_passthrough_transformer(self):
         """
         Tests that the PassthroughTransformer correctly stores the records.
         """
         mock_records = MagicMock(spec=PullRequestRecord)
-        transformer = PassThroughTransformer(mock_records)
+        transformer = PassthroughTransformer(mock_records)
         assert transformer.records == mock_records
